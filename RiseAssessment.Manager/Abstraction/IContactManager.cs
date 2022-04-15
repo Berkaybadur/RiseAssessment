036fd568e252da3de405b384cd59b3d2513f2554
berkay.badur <berkaybadur5@gmail.com>
2022-04-15 23:56:15 +0300

    Added Manager And MappingProfiles

---
+++ RiseAssessment.Manager/Abstraction/IContactManager.cs
@@ -0,0 +1,24 @@
+﻿using RiseAssesment.Infrastructure.CQRS.Commands.Request;
+using RiseAssesment.Infrastructure.CQRS.Commands.Response;
+using RiseAssesment.Infrastructure.CQRS.Common;
+using RiseAssesment.Infrastructure.CQRS.Queries;
+using RiseAssesment.Infrastructure.CQRS.Queries.Request;
+using RiseAssesment.Infrastructure.CQRS.Queries.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiseAssessment.Manager.Abstraction
+{
+    public interface IContactManager
+    {
+        Task<IEnumerable<ListContactQueryResponse>> GetAllContactAsync(ListContactQueryRequest requestModel);
+        Task<ContactQueryResponse> GetContactAsync(GetContactQueryRequest requestModel);
+        Task<CreateContactCommandResponse> CreateContactAsync(CreateContactCommandRequest requestModel);
+        Task<EmptyResponse?> UpdateContactAsync(UpdateContactCommandRequest requestModel);
+        Task<EmptyResponse?> DeleteContactAsync(DeleteContactCommandRequest requestModel);
+
+    }
+}
---
+++ RiseAssessment.Manager/DirectoryManager.cs
@@ -0,0 +1,48 @@
+﻿using MediatR;
+using RiseAssesment.Infrastructure.CQRS.Commands.Request;
+using RiseAssesment.Infrastructure.CQRS.Commands.Response;
+using RiseAssesment.Infrastructure.CQRS.Common;
+using RiseAssesment.Infrastructure.CQRS.Queries.Request;
+using RiseAssesment.Infrastructure.CQRS.Queries.Response;
+using RiseAssessment.Manager.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiseAssessment.Manager
+{
+    public class DirectoryManager : IDirectoryManager
+    {
+        private readonly IMediator _mediator;
+        public DirectoryManager(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        public async Task<CreateDirectoryCommandResponse> CreateDirectoryAsync(CreateDirectoryCommandRequest requestModel)
+        {
+            return await _mediator.Send(requestModel);
+        }
+
+        public async Task<EmptyResponse> DeleteDirectoryAsync(DeleteDirectoryCommandRequest requestModel)
+        {
+            return await _mediator.Send(requestModel);
+        }
+
+        public async Task<IEnumerable<ListDirectoryQueryResponse>> GetAllDirectoryAsync(ListDirectoryQueryRequest requestModel)
+        {
+            return await _mediator.Send(requestModel);
+        }
+
+        public async Task<DirectoryQueryResponse> GetDirectoryAsync(GetDirectoryQueryRequest requestModel)
+        {
+            return await _mediator.Send(requestModel);
+        }
+
+        public async Task<EmptyResponse> UpdateDirectoryAsync(UpdateDirectoryCommandRequest requestModel)
+        {
+            return await _mediator.Send(requestModel);
+        }
+    }
+}
