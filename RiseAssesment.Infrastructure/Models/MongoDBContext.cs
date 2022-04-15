9a17bdda3124ec1b157acb001727d30294677085
berkay.badur <berkaybadur5@gmail.com>
2022-04-15 22:28:31 +0300

    Added MongoDbContext

---
+++ RiseAssesment.Infrastructure/Models/MongoDBContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiseAssesment.Infrastructure.Models
+{
+    public class MongoDBContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IMongoDatabase _mongoDatabase;
+        public MongoDBContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            var connectionString = _configuration.GetValue<string>("MongoDBConfiguration:ConnectionString");
+            var db = _configuration.GetValue<string>("MongoDBConfiguration:Database");
+            var client = new MongoClient(connectionString);
+            _mongoDatabase = client.GetDatabase(db);
+        }
+        public IMongoCollection<Directory> Directory => _mongoDatabase.GetCollection<Directory>(nameof(Directory));
+        public IMongoCollection<Contact> Contact => _mongoDatabase.GetCollection<Contact>(nameof(Contact));
+
+    }
+}
